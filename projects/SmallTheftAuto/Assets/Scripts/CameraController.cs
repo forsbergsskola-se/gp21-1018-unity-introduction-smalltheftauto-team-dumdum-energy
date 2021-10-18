903890c4b1fa429a940298dc1c5c0514e66858c5
Isac Lagerström <isac.lagerstrom@student.forsbergsskola.se>
2021-10-18 16:45:11 +0200

    Car added plus minor changes

--- projects/SmallTheftAuto/Assets/Scripts/CameraController.cs
+++ projects/SmallTheftAuto/Assets/Scripts/CameraController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,29 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform player;
+    public Transform endMarker;
     public float delay;
     public Vector3 offset;
+    private float starttime;
+    private float journeyLength;
+    private void Start()
+    {
+        starttime = Time.time;
+        journeyLength = Vector3.Distance(player.position, endMarker.position);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        Vector3 dumDum = Vector3.Lerp(transform.position, player.position, delay);
-        transform.position = dumDum + offset;
+        float distCovered = (Time.time - starttime) * delay;
+        float fractionofJourney = distCovered / journeyLength;
+        //Vector3 dumDum = Vector3.Lerp(transform.position, player.position, delay);
+        //transform.position = dumDum + offset;
+        transform.position = Vector3.Lerp(player.position, endMarker.position, fractionofJourney);
+    }
+
+    private void FixedUpdate()
+    {
+        
     }
 }
---
+++ projects/SmallTheftAuto/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public float speed = 150.0f;
+    public float rotationSpeed = 10.0f;
+
+    private Rigidbody bil;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bil = GetComponent<Rigidbody>();
+        float force = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        force *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        // Move translation along the z-axis
+        bil.AddRelativeForce(0,0,force);
+
+        // Rotate around our y-axis
+        transform.Rotate(0, rotation, 0);
+    }
+}
