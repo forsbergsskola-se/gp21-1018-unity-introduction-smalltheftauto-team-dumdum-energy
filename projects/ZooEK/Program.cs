066bcc44161ddf66ff419a82b43a00a88be6ae75
xakzi <emanoel.kouriat@student.forsbergsskola.se>
2021-10-20 14:27:06 +0200

    Update Program.cs

--- projects/ZooEK/Program.cs
+++ projects/ZooEK/Program.cs
@@ -8,17 +8,22 @@ namespace ZooEK
         {
             Bear bear = new Bear();
             Donkey donkey = new Donkey();
+            Lion lion = new Lion();
             Clownfish clownfish = new Clownfish();
             Salmon salmon = new Salmon();
             Zoo<Mammal> animalZoo = new Zoo<Mammal>();
             Zoo<Fish> fishZoo = new Zoo<Fish>();
+            Zoo<Donkey> donkeyZoo = new Zoo<Donkey>();
+
 
             animalZoo.AddObject = bear;
+            animalZoo.AddObject = donkey;
+            animalZoo.AddObject = lion;
+
+            donkeyZoo.AddObject = donkey;
 
             fishZoo.AddObject = clownfish;
             fishZoo.AddObject = salmon;
-
-            Console.WriteLine();
         }
     }
 
