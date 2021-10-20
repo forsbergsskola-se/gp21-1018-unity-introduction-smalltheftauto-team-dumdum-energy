4fe045b6213339187050545578286ddda2757414
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2021-10-20 13:04:35 +0200

    Added the ZooJD exercise

---
+++ projects/ZooJD/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ZooJD
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+
+    class Animal
+    {
+        
+    }
+
+    class Mammal : Animal
+    {
+        
+    }
+
+    class Bear : Mammal
+    {
+        
+    }
+
+    class Donkey : Mammal
+    {
+        
+    }
+
+    class Lion : Mammal
+    {
+        
+    }
+
+    class Fish : Animal
+    {
+        
+    }
+
+    class Salmon : Fish
+    {
+        
+    }
+
+    class Clownfish : Fish
+    {
+        
+    }
+
+    class Student
+    {
+        
+    }
+}
