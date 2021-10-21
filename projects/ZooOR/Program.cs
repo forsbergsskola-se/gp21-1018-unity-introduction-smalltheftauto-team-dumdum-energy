e2126fe28687d261e86474801b3ac56a5ffc76cb
OlleKilgarrah <olle.reberg@student.forsbergsskola.se>
2021-10-21 11:09:43 +0200

    IT's a frickin zoo up in 'ere

--- projects/ZooOR/Program.cs
+++ projects/ZooOR/Program.cs
@@ -1,14 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ZooOR
 {
-    public class Zoo<Z>
+    class Zoo<TAnimal> where TAnimal : Animal, new() //Makes sure I can only add zoos with animals in them
     {
-        public void AddAnimal(Z Animal)
+        List<TAnimal> animals = new List<TAnimal>(); // This is array, How do me use diz?
+        
+        
+        public void AddAnimal(TAnimal animal)
             {
-    
+                // Increases size of array that holds TAnimals by one, whenever we AddAnimal
+                this.animals.Add(animal);
+                //Here we put it into new space created by array
+                //Since we start at 0, we need to subtract 1, this means the animal added is "put at the end"/sorted at the end.
+                //animals[^1] = animal;
             }
-        public Z AddType { get; set; }
+
+        public bool HasAnimal<TSpecies>() where TSpecies : Animal
+        {
+            
+        }
+        
     }
 
     public class Animal
@@ -21,32 +34,34 @@ namespace ZooOR
     {
         
     }
-    public class Beer : Animal
+    public class Bear : Mammal
     {
         
     }
-    public class Donkeh : Animal
+    public class Donkeh : Mammal
     {
         
     }
-    public class Simba : Animal
+    public class Simba : Mammal
     {
         
     }
-    public class FishnChips : Animal
+    public class Fishy : Animal
+    {
+        
+        
+    }
+    public class Salmon : Fishy
     {
         
     }
-    public class Salmonella : FishnChips
+    public class Nemo : Fishy
     {
-        
-    }
-    public class Nemo : FishnChips
-    {
+        Nemo nemo = new Nemo();
         
     }
 
-    public class StudenteAldente
+    public class Student
     {
         
     }
@@ -56,12 +71,20 @@ namespace ZooOR
     {
         static void Main(string[] args)
         {
-            Nemo nemo = new Nemo();
-            Zoo<FishnChips> fishyZoo = new Zoo<FishnChips>();
-            Zoo<Mammal> mammies = new Zoo<Mammal>();
-            Zoo<Donkeh> duncans = new Zoo<Donkeh>();
+            
+            Zoo<Fishy> fishyZoo = new Zoo<Fishy>();
+            fishyZoo.AddAnimal(new Fishy()); // Adds new Fish
+            fishyZoo.AddAnimal(new Nemo()); // Adds new clownfish
+            //Zoo<Student> studenTAnimal = new Zoo<Student>(); -> Can't add student Zoo(sadly)
+            Zoo<Simba> simbaZoo = new Zoo<Simba>();
+            simbaZoo.AddAnimal(new Simba());
+            simbaZoo.AddAnimal(new Simba());
+            
+            Zoo<Salmon> salmonZoo = new Zoo<Salmon>();
+            salmonZoo.HasAnimal<Salmon>(); //Here we make use of hasanimal public method. See method in Zoo class for more
+           // Console.WriteLine("This is a lie: "+fishyZoo.HasAnimal<Nemo>()); //Doesn't work for some reason
 
-            fishyZoo.AddType = nemo;
+
         }
     }
 }
