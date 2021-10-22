53c8dcec0900eaf4b3249744f9567cc0981f4d70
OlleKilgarrah <olle.reberg@student.forsbergsskola.se>
2021-10-22 13:50:03 +0200

    Healthbar added to main, added playerscript player takes dmg

---
+++ projects/SmallTheftAuto/Assets/Scripts/PlayerScripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+
+        if (currentHealth == 0)
+        {
+            IsPlayerDead();
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+
+    void IsPlayerDead()
+    {
+        
+    }
+}
