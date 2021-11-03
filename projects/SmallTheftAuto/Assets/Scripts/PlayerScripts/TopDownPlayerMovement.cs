67ca764df1ad739db3ad2a70c239787f11604221
OlleKilgarrah <olle.reberg@student.forsbergsskola.se>
2021-11-03 16:14:36 +0100

    Fixat TopDownPlayerMovement script

--- projects/SmallTheftAuto/Assets/Scripts/PlayerScripts/TopDownPlayerMovement.cs
+++ projects/SmallTheftAuto/Assets/Scripts/PlayerScripts/TopDownPlayerMovement.cs
@@ -7,6 +7,12 @@ public class TopDownPlayerMovement : MonoBehaviour
 {
     private InputHandler _input;
 
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private Camera camera;
+    [SerializeField] private bool rotateTowardsMouse;
+    
+
     private void Awake()
     {
         _input = GetComponent<InputHandler>();
@@ -16,7 +22,47 @@ public class TopDownPlayerMovement : MonoBehaviour
     void Update()
     {
         var targetVector = new Vector3(_input.InputVector.x, 0, _input.InputVector.y);
+        
         // Move in aim direction
-        // rotate in travel direction
+        var movementVector = MoveTowardTarget(targetVector);
+
+        if (!rotateTowardsMouse)
+        {
+            RotateTowardMovementVector(movementVector);
+        }
+        else
+        {
+            RotateTowardMouseVector();
+        }
+    }
+
+    private void RotateTowardMouseVector()
+    {
+        Ray ray = camera.ScreenPointToRay(_input.MousePosition);
+
+        if (!Physics.Raycast(ray, out RaycastHit hitinfo, maxDistance: 300f)) return;
+        var target = hitinfo.point;
+        target.y = transform.position.y;
+        transform.LookAt(target);
+    }
+
+    private void RotateTowardMovementVector(Vector3 movementVector)
+    {
+        if (movementVector.magnitude == 0)
+        {
+            return;
+        }
+        var rotation = Quaternion.LookRotation(movementVector);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotateSpeed);
+    }
+
+    private Vector3 MoveTowardTarget(Vector3 targetVector)
+    {
+        var speed = moveSpeed * Time.deltaTime;
+
+        targetVector = Quaternion.Euler(0, camera.gameObject.transform.eulerAngles.y, 0) * targetVector;
+        var targetPosition = transform.position + targetVector * speed;
+        transform.position = targetPosition;
+        return targetVector;
     }
 }
