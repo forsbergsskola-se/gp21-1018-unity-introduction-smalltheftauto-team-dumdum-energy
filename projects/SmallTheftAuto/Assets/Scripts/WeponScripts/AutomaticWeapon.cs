a9de31f8effdb0f929a53916208b287612383187
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2021-11-04 15:10:40 +0100

    Weapons now require reload

--- projects/SmallTheftAuto/Assets/Scripts/WeponScripts/AutomaticWeapon.cs
+++ projects/SmallTheftAuto/Assets/Scripts/WeponScripts/AutomaticWeapon.cs
@@ -12,16 +12,31 @@ public class AutomaticWeapon : MonoBehaviour
     private float fireRate = 10f;
     private bool shooting = false;
     private float bulletForce = 200f;
-    public int slot = 2;
+    private float reloadTime = 3f;
+    private bool isReloading = false;
+    private int maxAmmo = 32;
+    private int currentAmmo;
+    
 
     private void Start()
     {
+        currentAmmo = maxAmmo;
         spawnPoint = GetComponentInChildren<Transform>().Find("FirePoint");
         InvokeRepeating(nameof(Shoot), 0,1/fireRate);
     }
 
     private void Update()
     {
+        if (isReloading)
+        {
+            return;
+        }
+
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
         shooting = false;
         if (Input.GetButton("Fire1"))
         {
@@ -35,8 +50,17 @@ public class AutomaticWeapon : MonoBehaviour
         {
             return;
         }
+        currentAmmo--;
         GameObject bullet = Instantiate(bulletprefab, spawnPoint.position, spawnPoint.rotation);
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
         rb.AddForce(spawnPoint.forward*bulletForce, ForceMode.Impulse);
+    }
+    
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        yield return new WaitForSeconds(reloadTime);
+        currentAmmo = maxAmmo;
+        isReloading = false;
     }
 }
--- projects/SmallTheftAuto/Assets/Scripts/WeponScripts/Pistol.cs
+++ projects/SmallTheftAuto/Assets/Scripts/WeponScripts/Pistol.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEditor.Search;
 using UnityEngine;
 
@@ -10,18 +11,29 @@ public class Pistol : MonoBehaviour
     public GameObject bulletprefab;
     public float Damage = 15;
 
-    public int slot = 1;
-
+    private int maxAmmo = 7;
+    private int currentAmmo;
+    private float reloadTime = 1f;
     private float bulletForce = 200f;
+    private bool isReloading = false;
 
     private void Start()
     {
         spawnPoint = GetComponentInChildren<Transform>().Find("FirePoint");
+        currentAmmo = maxAmmo;
     }
 
     private void Update()
     {
-        
+        if (isReloading)
+        {
+            return;
+        }
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
@@ -30,8 +42,17 @@ public class Pistol : MonoBehaviour
     
     void Shoot()
     {
+        currentAmmo--;
         GameObject bullet = Instantiate(bulletprefab, spawnPoint.position, spawnPoint.rotation);
         Rigidbody rb = bullet.GetComponent<Rigidbody>();
         rb.AddForce(spawnPoint.forward*bulletForce, ForceMode.Impulse);
     }
+
+        IEnumerator Reload()
+        {
+            isReloading = true;
+            yield return new WaitForSeconds(reloadTime);
+            currentAmmo = maxAmmo;
+            isReloading = false;
+        }
 }
