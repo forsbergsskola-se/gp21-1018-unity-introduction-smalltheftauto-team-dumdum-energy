f80636e2fb2b93f215b9d8bb259180bf724db12a
OlleKilgarrah <olle.reberg@student.forsbergsskola.se>
2021-11-04 15:55:36 +0100

    Created Knife prefab and started on knife script

---
+++ projects/SmallTheftAuto/Assets/Scripts/WeponScripts/Knife.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        //Add on click for it to perform slash animation
+        //Add on slashhit - > deal damage, or whatever
+    }
+}
--- projects/SmallTheftAuto/Assets/Scripts/WeponScripts/WeaponHolder.cs
+++ projects/SmallTheftAuto/Assets/Scripts/WeponScripts/WeaponHolder.cs
@@ -8,12 +8,16 @@ public class WeaponHolder : MonoBehaviour
 {
     private Pistol _pistol;
     private AutomaticWeapon _auto;
+    private Knife _knife;
+    public GameObject knife;
 
 
     private void Start()
     {
        _pistol = GetComponent<Pistol>();
        _auto = GetComponent<AutomaticWeapon>();
+       _knife = GetComponent<Knife>();
+       _knife.enabled = false;
        _pistol.enabled = false;
        _auto.enabled = false;
     }
@@ -24,12 +28,22 @@ public class WeaponHolder : MonoBehaviour
         {
             _pistol.enabled = true;
             _auto.enabled = false;
+            _knife.enabled = false;
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             _pistol.enabled = false;
+            _knife.enabled = false;
             _auto.enabled = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            _pistol.enabled = false;
+            _knife.enabled = true;
+            FindObjectOfType<Knife>().gameObject.SetActive(true);
+            
+            _auto.enabled = false;
         }
     }
 }
