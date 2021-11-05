5df59d40a1af3b2c4c169540fa4faa6439e504a9
OlleKilgarrah <olle.reberg@student.forsbergsskola.se>
2021-11-05 11:34:28 +0100

    Added "enemy"

--- projects/SmallTheftAuto/Assets/Scripts/WeponScripts/Knife.cs
+++ projects/SmallTheftAuto/Assets/Scripts/WeponScripts/Knife.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -26,5 +27,14 @@ public class Knife : MonoBehaviour
     public void Slash() 
     {
         _animator.SetTrigger("Attack");
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("I hit!" + col.name);
+        if (col.tag == "Enemy")
+        {
+            
+        }
     }
 }
