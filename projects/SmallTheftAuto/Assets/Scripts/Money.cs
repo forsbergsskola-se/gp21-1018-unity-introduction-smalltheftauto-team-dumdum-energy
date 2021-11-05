de28d34fb0b6753147196d70ab52c2c1cbdf4742
Jesper Danielsson <jesper.danielsson@student.forsbergsskola.se>
2021-11-05 12:42:18 +0100

    Money update

---
+++ projects/SmallTheftAuto/Assets/Scripts/Money.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Money : MonoBehaviour
+{
+
+    public static int scoreValue = 0;
+    Text _score;
+
+   
+
+    void Start()
+    {
+        _score = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        _score.text = "Score: " + scoreValue;
+    }
+    public void SetScore(int score)
+    {
+        _score.text = "Score: " + score;
+    }
+}
